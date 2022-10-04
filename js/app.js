3e4f5ab8671a4fb2619e8797edd2b72c88922967
Alvaritto951 <alvaritto951@gmail.com>
2022-10-04 20:45:54 +0200

    Añadimos filas a una tabla

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 // Comentario de línea
 /* Comentario de bloque */
 
-function cuentaClicks(event) {
-    let input = document.querySelector("#entrada").value
-    alert(input)
-    // TODO: Crear un párrafo debajo del botón con el texto de input
+function addRow(event) {
+    const texto = document.querySelector("#entrada").value //Selecciona la entrada y su valor (el texto) y nos lo muestra. Te devuelve el nodo (objeto)
+    const tabla = document.querySelector("#la_tabla")
+    // TODO: Crear una fila a la tabla la_tabla debajo del botón con el texto de input
+    const fila = document.createElement("tr")
+    const celda = document.createElement("td")
+    celda.innerHTML = texto //Lee lo que se escribe dentro de cada celda
+
+    fila.appendChild(celda)
+    tabla.appendChild(fila)
 }
 
-window.onload = function() {
+window.onload = function() { //Objeto ventana: Equivale a la pestaña del navegador
     let elboton = document.querySelector("#crear")
-    elboton.addEventListener("click", cuentaClicks)
+    elboton.addEventListener("click", addRow)
 }
